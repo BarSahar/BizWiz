88832033354720df5d0dcab1bc2a044639673650
Bar Sahar <bsahar1990@gmail.com>
2016-04-07 22:10:04 +0300

    added class and view

---
+++ BizWizProj/BizWizProj/Models/closedShift.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BizWizProj.Models
+{
+    public class closedShift
+    {
+        [Key]
+        public int ID { get; set; }
+        public int dayIndex { get; set; }
+        public int shiftIndex { get; set; }
+        public DateTime weekDate { get; set; }
+        public TimeSpan startHour { get; set; }
+        public TimeSpan endHour { get; set; }
+        public BizUser shiftManager { get; set; }
+        public List<BizUser> workers { get; set; }
+    }
+}
