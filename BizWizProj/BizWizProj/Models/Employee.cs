d1a2a61d1ad7b5246b652d8791a2cce8262b1b03
Bar Sahar <bsahar1990@gmail.com>
2016-04-07 18:48:33 +0300

    wellow

---
+++ BizWizProj/BizWizProj/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BizWizProj.Models
+{
+    public class Employee:User
+    {
+        string Name;
+
+    }
+}
---
+++ BizWizProj/BizWizProj/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BizWizProj.Models
+{
+    public class User
+    {
+        int ID { get; set; }
+        string userName { get; set; }
+        string password { get; set; }
+        string email { get; set; }
+        string phoneNumber { get; set; }
+        DateTime startDate { get; set; }
+    }
+}
