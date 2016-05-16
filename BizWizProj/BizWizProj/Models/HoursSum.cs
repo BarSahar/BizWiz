07a4c69d8672a7ef8367c30b5f48a8e0b4d91ccc
yoni.hrr <yoni.hrr@gmail.com>
2016-05-16 11:35:25 +0300

    HoursSum.cs - Remove Test From Code CloseShiftController -> view Index - reduce <br>

--- BizWizProj/BizWizProj/Models/HoursSum.cs
+++ BizWizProj/BizWizProj/Models/HoursSum.cs
@@ -18,49 +18,6 @@ namespace BizWizProj.Models
         {
 
             var counter = 0;
-
-            BizUser e1 = new BizUser() { ID = 1, Password = "1", FullName = "1", Email = "yoni@gmail.com", PhoneNumber = "054-2290705", EmployeeType = "4" };
-            BizUser e2 = new BizUser() { ID = 2, Password = "2", FullName = "bar", Email = "bar@gmail.com", PhoneNumber = "054-2290706", EmployeeType = "1" };
-            BizUser e3 = new BizUser() { ID = 3, Password = "3", FullName = "avi", Email = "avi@gmail.com", PhoneNumber = "054-2290707", EmployeeType = "1" };
-            BizUser e4 = new BizUser() { ID = 4, Password = "4", FullName = "elisha", Email = "elisha@gmail.com", PhoneNumber = "054-2290708", EmployeeType = "1" };
-            BizUser e5 = new BizUser() { ID = 5, Password = "5", FullName = "darsher", Email = "darsher@gmail.com", PhoneNumber = "054-2290709", EmployeeType = "1" };
-
-            List<BizUser> l1 = new List<BizUser>();
-            List<BizUser> l2 = new List<BizUser>();
-            l1.Add(e1);
-            l1.Add(e2);
-            l1.Add(e3);
-            l1.Add(e4);
-            l1.Add(e5);
-
-            l2.Add(e1);
-            l2.Add(e2);
-            l2.Add(e3);
-            l2.Add(e4);
-            l2.Add(e5);
-
-
-            db.SaveChanges();
-            db.BizUsers.AddRange(l1);
-            db.BizUsers.AddRange(l2);
-
-            List<BizUser> userList = db.BizUsers.ToList();
-
-            ClosedShift s1 = new ClosedShift() { WeekDate = DateTime.Now, Start = DateTime.Now, End = DateTime.Now.AddHours(3) };
-            ClosedShift s2 = new ClosedShift() { WeekDate = DateTime.Now, Start = DateTime.Now, End = DateTime.Now.AddHours(3) };
-
-            s1.Workers = new List<BizUser>();
-            s2.Workers = new List<BizUser>();
-
-            s1.Workers.AddRange(l1);
-            s2.Workers.AddRange(l2);
-
-            List<ClosedShift> cl = new List<ClosedShift>();
-            cl.Add(s1);
-            cl.Add(s2);
-
-            db.ShiftHistory.Add(s1);
-            db.SaveChanges();
             List<ClosedShift> shiftList = db.ShiftHistory.Local.ToList();
             shiftList.Reverse(); //start from the end
             if (shiftList != null)
