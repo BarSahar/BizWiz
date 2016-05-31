a642d8f2694679a4cf30a5aed39095aa2f775754
Vasserman2000 <vasserman2000@gmail.com>
2016-05-31 10:11:10 +0300

    bla

--- BizWizProj/BizWizProj/Models/modelShift.cs
+++ BizWizProj/BizWizProj/Models/modelShift.cs
@@ -12,6 +12,7 @@ namespace BizWizProj.Models
         public int ID { get; set; }
         public int DayIndex { get; set; }
         public int ShiftIndex { get; set; }
+        [Range(1, Int32.MaxValue)]
         public int NumOfEmployees { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
