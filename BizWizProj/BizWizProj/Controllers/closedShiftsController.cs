cfb542fa95119c6e5ca7fbb6d9b7a45ad0562621
Avi Hazan <avihazan22@gmail.com>
2016-06-12 09:29:52 +0300

    nice

--- BizWizProj/BizWizProj/Controllers/closedShiftsController.cs
+++ BizWizProj/BizWizProj/Controllers/closedShiftsController.cs
@@ -19,10 +19,10 @@ namespace BizWizProj.Controllers
     [MyAuthorize]
     public class ClosedShiftsController : Controller
     {
-        //Avi
+        //Avi-Searcing for Employees That is not Working on Some Shift 
         #region
         public static int ShiftId;
-        public string OpenModelPopup() //Searcing for Employees That is not Working on Some Shift 
+        public string OpenModelPopup() 
         {
             List<BizUser> BizzUsersList = db.BizUsers.ToList(); //loading all Users from db 
             List<BizUser> NotWorkingUsers = new List<BizUser>(); //List of the Workers That not work on this Shift
